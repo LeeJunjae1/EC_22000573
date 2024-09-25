1a9808407d82fa023a6de2c44e32d0036dd118d4
LeeJunjae1 <145029385+LeeJunjae1@users.noreply.github.com>
2024-09-25 21:00:55 +0900

    Update ecEXTI2.c

--- lib/ecEXTI2.c
+++ lib/ecEXTI2.c
@@ -5,9 +5,9 @@
 
 void EXTI_init(PinName_t pinName, int trig_type,int priority){
 
-	GPIO_Typedef *port;
-	unsigned int pin;
-	ecPinmap(pinName,&port,&pin);
+	GPIO_TypeDef * Port;
+	unsigned int Pin;
+	ecPinmap(pinName,&Port,&Pin);
 	// SYSCFG peripheral clock enable	
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;		
 	
@@ -17,52 +17,67 @@ void EXTI_init(PinName_t pinName, int trig_type,int priority){
 	else if	(Port == GPIOB) EXTICR_port = 1;
 	else if	(Port == GPIOC) EXTICR_port = 2;
 	else if	(Port == GPIOD) EXTICR_port = 3;
-	else 										EXTICR_port = 4;
+	else if	(Port == GPIOE) EXTICR_port = 4;
+	else 										EXTICR_port = 7;
 	
-	SYSCFG->EXTICR[_______] &= ________________;			// clear 4 bits
-	SYSCFG->EXTICR[_______] |= ________________;			// set 4 bits
+	unsigned int pin=Pin%4;// use pin number of EXTICR
+	unsigned int Re_num=Pin/4;//EXTICR number
+	SYSCFG->EXTICR[Re_num] &= ~(0xF<<(pin*4));			// clear 4 bits
+	SYSCFG->EXTICR[Re_num] |= (EXTICR_port<<(pin*4));			// set 4 bits
 	
 	// Configure Trigger edge
-	if (trig_type == FALL) EXTI->FTSR |= _______;   // Falling trigger enable 
-	else if	(trig_type == RISE) EXTI->RTSR |= _______;   // Rising trigger enable 
+	if (trig_type == FALL) EXTI->FTSR |= (1<<Pin);   // Falling trigger enable 
+	else if	(trig_type == RISE) EXTI->RTSR |= (1<<Pin);   // Rising trigger enable 
 	else if	(trig_type == BOTH) {			// Both falling/rising trigger enable
-		EXTI->RTSR |= _______; 
-		EXTI->FTSR |= _______;
+		EXTI->RTSR |= (1<<Pin); 
+		EXTI->FTSR |=(1<<Pin);
 	} 
 	
 	// Configure Interrupt Mask (Interrupt enabled)
-	EXTI->IMR  |= _______;     // not masked
+	EXTI->IMR  |= (1<<Pin);     // not masked
 	
 	
 	// NVIC(IRQ) Setting
 	int EXTI_IRQn = 0;
 	
-	if (Pin < 5) 	EXTI_IRQn = _______;
-	else if	(Pin < 10) 	EXTI_IRQn = _______;
-	else 			EXTI_IRQn = _______;
+	if (Pin < 5) 	EXTI_IRQn = Pin+6;//Poistion
+	else if	(Pin < 10) 	EXTI_IRQn = EXTI9_5_IRQn;
+	else 			EXTI_IRQn = EXTI15_10_IRQn;
 								
-	NVIC_SetPriority(EXTI_IRQn, _______);	// EXTI priority
+	NVIC_SetPriority(EXTI_IRQn, priority);	// EXTI priority
 	NVIC_EnableIRQ(EXTI_IRQn); 	// EXTI IRQ enable
 }
 
 
 void EXTI_enable(PinName_t pinName) {
 	//Todo Port, Pin Configuration 
-	EXTI->IMR __= _______;     // not masked (i.e., Interrupt enabled)
+	GPIO_TypeDef * Port;
+	unsigned int pin;
+	ecPinmap(pinName,&Port,&pin);
+	EXTI->IMR |= (1<<pin);     // not masked (i.e., Interrupt enabled)
 }
 void EXTI_disable(PinName_t pinName) {
 	//Todo Port, Pin Configuration 
-	EXTI->IMR __= _______;     // masked (i.e., Interrupt disabled)
+	GPIO_TypeDef * Port;
+	unsigned int pin;
+	ecPinmap(pinName,&Port,&pin);
+	EXTI->IMR &=~(1<<pin);     // masked (i.e., Interrupt disabled)
 }
 
 uint32_t is_pending_EXTI(PinName_t pinName) {
 	//Todo Port, Pin Configuration 
-	uint32_t EXTI_PRx = _______;     	// check  EXTI pending 	
-	return ((EXTI->PR __ _______) == _______);
+	GPIO_TypeDef * Port;
+	unsigned int pin;
+	ecPinmap(pinName,&Port,&pin);
+	uint32_t EXTI_PRx = (1<<pin);     	// check  EXTI pending 	
+	return ((EXTI->PR & EXTI_PRx) == EXTI_PRx);
 }
 
 
 void clear_pending_EXTI(PinName_t pinName) {
 	//Todo Port, Pin Configuration 
-	EXTI->PR __= _______;     // clear EXTI pending 
+	GPIO_TypeDef * Port;
+	unsigned int pin;
+	ecPinmap(pinName,&Port,&pin);
+	EXTI->PR|= (1<<pin);     // clear EXTI pending 
 }
