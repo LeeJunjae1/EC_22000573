633ac77f973187e3ffe29978f106b8835904d845
LeeJunjae1 <145029385+LeeJunjae1@users.noreply.github.com>
2024-10-12 16:07:12 +0900

    Add files via upload

---
+++ lib/ecSTM32F4v2.h
@@ -0,0 +1,43 @@
+//ecSTM32F4v2.h
+
+
+/**
+******************************************************************************
+* @course   Embedded Controller- HGU
+* @author	iiLAB
+* @mod		2024-9-27 by JunjaeLee
+* @brief	STM32F411 Library for EC
+*
+******************************************************************************
+*/
+
+//ECSTM32F4V2
+#ifndef __EC_STM_H
+#define __EC_STM_H
+
+// STM built-In Library
+#include "stm32f4xx.h"
+#include "stm32f411xe.h"
+#include "math.h"
+
+// EC course Library
+#include "ecPinNames.h"
+#include "ecRCC2.h"
+#include "ecGPIO2.h"
+#include "ecEXTI2.h"
+#include "ecSysTick2.h"
+#include "MCU_init.h"//MCU init
+#include "ecTIM2.h"
+#include "ecPWM2.h"
+//#include "ecStepper2.h"
+//#include "ecADC2.h"
+#include "ecICAP2.h"
+#include "ecUART2_simple.h"
+
+
+#endif
+
+
+
+
+
